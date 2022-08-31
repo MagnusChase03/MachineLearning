d1af575004d4455334afc981e6ac5a8d180e55dc
Zachary <zachness2003@gmail.com>
2022-08-31 16:27:28 -0500

    Starting learning tensorflow from google

---
+++ Tensorflow/LinearRegression/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+# Cleaner display
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
