c4e1c5ea229a4af6e1c02522bde8cd9156d39b5d
Zachary <zachness2003@gmail.com>
2023-02-02 14:05:07 -0600

    Init of network

--- StockMarket/NeuralNetwork.py
+++ StockMarket/NeuralNetwork.py
@@ -1,3 +1,18 @@
+import numpy as np
+import random
+
 class NeuralNetwork:
-    def __init__(self):
-        pass
+    def __init__(self, inputNum, hiddenLayerNum, outputNum):
+        
+        # Create default layers
+        self.inputs = np.zeros(inputNum)
+        self.hiddenLayer = np.zeros((2, hiddenLayerNum))
+        self.outputs = np.zeros(outputNum)
+
+        # Random Weights
+        self.weights = []
+        self.weights.append(np.random.rand(inputNum, hiddenLayerNum))
+        self.weights.append(np.random.rand(hiddenLayerNum, hiddenLayerNum))
+        self.weights.append(np.random.rand(hiddenLayerNum, hiddenLayerNum))
+        self.weights.append(np.random.rand(hiddenLayerNum, outputNum))
+        
--- StockMarket/main.py
+++ StockMarket/main.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
+
 from NeuralNetwork import NeuralNetwork
+
+def main():
+    load_dotenv()
+
+    bot = NeuralNetwork(5, 6, 5)
+    print(bot.weights)
+
+main()
