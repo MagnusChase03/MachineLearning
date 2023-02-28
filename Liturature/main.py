283b7edccf78e23833ee495e496e712bfcf44985
Zachary <zachness2003@gmail.com>
2023-02-28 15:33:58 -0600

    Init

--- Liturature/main.py
+++ Liturature/main.py
@@ -1,36 +1,81 @@
 import numpy as np
 
-from NeuralNetwork import NeuralNetwork
+def onehot(inputs, size):
+    newMatrix = []
+    for num in inputs:
+        array = np.zeros((1, size))
+        array[0][num] = 1
+        newMatrix.append(array[0])
 
-dictionary = []
-for i in range(65, 65 + 26):
-    dictionary.append(chr(i))
+    return np.array(newMatrix)
 
-for i in range(97, 97 + 26):
-    dictionary.append(chr(i))
-
-dictionary.append('.')
-dictionary.append('\'')
-dictionary.append(' ')
-dictionary.append('\n')
-
-
-def encode(string):
-    encoding = []
-    for char in string:
-        if char in dictionary:
-            encoding.append(dictionary.index(char))
-
-    return encoding
-
-def decode(encoding):
-    string = ""
-    for index in encoding:
-        string += dictionary[index]
-
-    return string
 
 def main():
-    network = NeuralNetwork(8, 6, 1, 0.01);
+    words = open("words.txt", "r").read().splitlines()
+
+    N = np.zeros((27, 27), dtype=np.int32)
+    chars = sorted(set("".join(words)))
+
+    stoi = {s:i+1 for i,s in enumerate(chars)}
+    stoi["."] = 0
+
+    itos = {i:s for s,i in stoi.items()}
+
+    for w in words:
+        ch = ["."] + list(w) + ["."]
+        for c1, c2 in zip(ch, ch[1:]):
+            x = stoi[c1]
+            y = stoi[c2]
+        
+            N[x, y] += 1
+
+    P = N + 1.0
+    P /= P.sum(axis=1).reshape((27, 1))
+    g = np.random.default_rng()
+
+    for i in range(0, 5):
+        o = []
+        ix = 0
+        while True:
+            ix = g.multinomial(1, P[ix], size=1)[0].tolist().index(1)
+            if ix == 0:
+                break
+            o.append(itos[ix])
+
+        print("".join(o))
+
+    logLiklyhood = 0.0
+    n = 0
+    for w in words:
+        ch = ["."] + list(w) + ["."]
+        for c1, c2 in zip(ch, ch[1:]):
+            x = stoi[c1]
+            y = stoi[c2]
+            logLiklyhood += np.log(P[x, y])
+            n += 1
+
+    print(-logLiklyhood / n)
+
+    inputs = []
+    outputs = []
+
+    for w in words:
+        ch = ["."] + list(w) + ["."]
+        for c1, c2 in zip(ch, ch[1:]):
+            x = stoi[c1]
+            y = stoi[c2]
+            
+            inputs.append(x)
+            outputs.append(y)
+
+    inputs = onehot(inputs, 27)
+    weights = np.random.rand(27, 27)
+    
+    pred = inputs.dot(weights)
+    pred.reshape(len(inputs), 27)
+    logits = np.exp(pred)
+    prob = logits / logits.sum(axis=1).reshape(len(inputs), 1)
+
+    print(prob[0])
 
 main()
